0412f47caf13c96c17ecc630bc9c764d52b896d1
PATRICIA FERREIRA DE SOUSA <65580919+paty127@users.noreply.github.com>
2022-09-10 22:33:06 -0300

    Estrutura e comandos personalizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,8 +9,10 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+
+//const { method } = require("cypress/types/bluebird")
+
+
 //
 //
 // -- This is a child command --
@@ -23,3 +25,70 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('postarUsuarioSemSucesso', () => { 
+    cy.request({
+        method: 'POST',
+        url: '/usuarios',
+        failOnStatusCode: false,
+        body: {
+            "nome": "Fulano da Silva",
+            "email": "fulano@qa.com",
+            "password": "teste",
+            "administrador": "true",
+        }
+        
+    })
+})
+Cypress.Commands.add('rest', (method ='GET', url = '/', body = null, failOnStatusCode = false ) => {
+    return cy.request({
+        method: method,
+        url: url,   
+        failOnStatusCode: failOnStatusCode,
+        body: body
+    })
+})
+Cypress.Commands.add('logar', (email, senha)=> {
+    return cy.request({
+        method: 'POST',
+        url: '/login',   
+        failOnStatusCode: false,
+        body: {
+            "email": email,
+            "password": senha
+
+        }
+    })  
+}) 
+Cypress.Commands.add('buscarUsuarioParaLogin', () => {
+    cy.rest('GET', '/usuarios').then( res => {
+        expect(res.body).to.haveOwnProperty('usuarios')
+        return {
+            email: res.body.usuarios[0].email,
+            senha: res.body.usuarios[0].password
+        }
+    }) 
+}) 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
